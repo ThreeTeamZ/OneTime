5bbe345724224d5e2f95fad7401cb018d2c64810
superGG1990 <superGG1990@163.com>
2018-04-24 20:26:57 +0800

    Y

--- GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/MainActivity.java
+++ GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这是一个钟
+        //我是杨帆
     }
 }
