4ad4ee03da53cec6a3ccfd447f7d5dc95fad54f4
zxw2008 <1797944974@qq.com//git>
2018-04-25 20:20:31 +0800

    Z

--- GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/api/Api.java
+++ GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/api/Api.java
@@ -1,10 +1,13 @@
 package view.zhengxiaolong.bw.com.gittext.api;
 
+import retrofit2.http.GET;
+
 /**
  * Created by lenovo on 2018/4/25.
  */
 
 public class Api {
-
+    //获取视频 https://www.zhaoapi.cn/quarter/getVideos
+    public static final String HOME = " https://www.zhaoapi.cn";
 
 }
--- GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/api/ApiService.java
+++ GitText/app/src/main/java/view/zhengxiaolong/bw/com/gittext/api/ApiService.java
@@ -1,8 +1,17 @@
 package view.zhengxiaolong.bw.com.gittext.api;
 
+import io.reactivex.Flowable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import view.zhengxiaolong.bw.com.gittext.bean.GetMVInfo;
+
 /**
  * Created by lenovo on 2018/4/25.
  */
 
 public interface ApiService {
+
+
+    @GET("/quarter/getVideos")
+    Flowable<GetMVInfo> getMv(@Query("path") String path,@Query("uid") String uid,@Query("appVersion") String appVersion);
 }
